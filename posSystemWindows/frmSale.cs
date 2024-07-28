e1e51dbe7c6c5e1fb4479a3668a6e2456b5e86b7
Aung Pyae Cho <aungpyaecho@gmail.com>
2024-07-28 21:48:27 +0630

    update search

--- posSystemWindows/frmSale.cs
+++ posSystemWindows/frmSale.cs
@@ -18,11 +18,13 @@ namespace posSystemWindows
             InitializeComponent();
             _dapperService = new DapperService(ConnectionStrings._sqlConnectionStringBuilder.ConnectionString);
             dgvItems.AutoGenerateColumns = false;
+            dgvCart.AutoGenerateColumns = false;
         }
 
         private void frmSale_Load(object sender, EventArgs e)
         {
             //loadData();
+            LoadCategories();
         }
 
         private void loadData()
@@ -53,6 +55,105 @@ namespace posSystemWindows
             this.Close();
         }
 
+        public void clear()
+        {
+            txtItemName.Text = string.Empty;
+            txtItemPrice.Text = string.Empty;
+            txtWholeSalePrice.Text = string.Empty;
+            txtCategory.Text = string.Empty;
+            txtSubCategory.Text = string.Empty;
+            txtBrand.Text = string.Empty;
+            txtSubBrand.Text = string.Empty;
+            txtQuantity.Text = "1";
+            txtByName.Text = string.Empty;
+        }
+
+        private void btnShowAll_Click(object sender, EventArgs e)
+        {
+            loadData();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            SearchItem();
+        }
+
+        private void SearchItem()
+        {
+            string itemName = txtByName.Text;
+            string itemBarcode = txtBarcode.Text;
+            string categoryCode = cboCategories.SelectedValue.ToString();
+
+            var parameters = new
+            {
+                itemName = string.IsNullOrEmpty(itemName) ? (string?)null : itemName,
+                itemBarcode = string.IsNullOrEmpty(itemBarcode) ? (string?)null : itemBarcode,
+                catCode = string.IsNullOrEmpty(categoryCode) ? (string?)null : categoryCode
+            };
+
+            if (!string.IsNullOrEmpty(itemName) || !string.IsNullOrEmpty(itemBarcode))
+            {
+                // Fetch only one row for item name or barcode search
+                var dataRow = _dapperService.QueryDataRow(Queries.GetItemBy, parameters);
+
+                if (dataRow != null)
+                {
+                    // Convert DataRow to a DataTable with a single row to bind to DataGridView
+                    var singleRowTable = dataRow.Table.Clone(); // Create an empty DataTable with the same schema
+                    singleRowTable.ImportRow(dataRow); // Import the DataRow
+                    dgvItems.DataSource = singleRowTable;
+                }
+                else
+                {
+                    // Handle case when no item is found
+                    MessageBox.Show("No item found with the specified criteria.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+            else if (!string.IsNullOrEmpty(categoryCode))
+            {
+                // Fetch all rows for category search
+                var dataTable = _dapperService.QueryDataTable(Queries.GetItemBy, parameters);
+
+                if (dataTable.Rows.Count > 0)
+                {
+                    dgvItems.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No item found with the specified criteria.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+            }
+        }
+
+
+        private void SearchUnkownValue()
+        {
+            string itemName = txtByName.Text;
+            string itemBarcode = txtBarcode.Text;
+
+            var parameters = new
+            {
+                itemName = string.IsNullOrEmpty(itemName) ? (string?)null : itemName,
+                itemBarcode = string.IsNullOrEmpty(itemBarcode) ? (string?)null : itemBarcode
+            };
+
+            var dataTable = _dapperService.QueryDataTable(Queries.GetItemByNameOrBarcodeUnKown, parameters);
+
+            if (dataTable.Rows.Count > 0)
+            {
+                dgvItems.DataSource = dataTable;
+            }
+            else
+            {
+                MessageBox.Show("No item found with the specified criteria.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+
+        private void btnSearchRange_Click(object sender, EventArgs e)
+        {
+            SearchUnkownValue();
+        }
+
         private void dgvItems_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             if (e.RowIndex >= 0)
@@ -77,43 +178,6 @@ namespace posSystemWindows
                 txtSubCategory.Text = subCatName;
                 txtBrand.Text = brandName;
                 txtSubBrand.Text = subBrandName;
-
-                //decimal salePrice;
-                //if (chbWholeSale.Checked is true)
-                //{
-                //    salePrice = itemWholeSalePrice;
-                //}
-                //else
-                //{
-                //    salePrice = itemPrice;
-                //}
-                //decimal netAmount = salePrice * quantity;
-                // Add the item to dgvCheckout with a default quantity of 1
-
-            }
-        }
-
-        private void dgvCart_CellEndEdit(object sender, DataGridViewCellEventArgs e)
-        {
-            // Ensure the edited cell is in the quantity column
-            if (e.ColumnIndex == dgvCart.Columns["quantity"].Index)
-            {
-                DataGridViewRow row = dgvCart.Rows[e.RowIndex];
-                decimal itemPrice = Convert.ToDecimal(row.Cells["Column8"].Value);
-                int quantity;
-
-                // Try to parse the quantity value
-                if (int.TryParse(row.Cells["quantity"].Value.ToString(), out quantity))
-                {
-                    // Calculate the total price and update the total price cell
-                    row.Cells["netAmount"].Value = itemPrice * quantity;
-                }
-                else
-                {
-                    // If parsing fails, reset the quantity to 1 and update the total price
-                    row.Cells["quantity"].Value = 1;
-                    row.Cells["netAmount"].Value = itemPrice;
-                }
             }
         }
 
@@ -145,82 +209,51 @@ namespace posSystemWindows
             clear();
         }
 
-        public void clear()
+        private void dgvCart_CellEndEdit(object sender, DataGridViewCellEventArgs e)
         {
-            txtItemName.Text = string.Empty;
-            txtItemPrice.Text = string.Empty;
-            txtWholeSalePrice.Text = string.Empty;
-            txtCategory.Text = string.Empty;
-            txtSubCategory.Text = string.Empty;
-            txtBrand.Text = string.Empty;
-            txtSubBrand.Text = string.Empty;
-            txtQuantity.Text = "1";
-            txtByName.Text = string.Empty;
-        }
-
-        private void btnShowAll_Click(object sender, EventArgs e)
-        {
-            loadData();
-        }
-
-        private void btnSearch_Click(object sender, EventArgs e)
-        {
-            SearchItem();
-        }
-
-        private void SearchItem()
-        {
-            string itemName = txtByName.Text;
-            string itemBarcode = txtBarcode.Text;
-
-            var parameters = new
+            // Ensure the edited cell is in the quantity column
+            if (e.ColumnIndex == dgvCart.Columns["quantity"].Index)
             {
-                itemName = string.IsNullOrEmpty(itemName) ? (string?)null : itemName,
-                itemBarcode = string.IsNullOrEmpty(itemBarcode) ? (string?)null : itemBarcode
-            };
+                DataGridViewRow row = dgvCart.Rows[e.RowIndex];
+                decimal itemPrice = Convert.ToDecimal(row.Cells["colItemPrice"].Value);
+                int quantity;
 
-            var dataRow = _dapperService.QueryDataRow(Queries.GetItemByNameOrBarcode, parameters);
-
-            if (dataRow != null)
-            {
-                // Convert DataRow to a DataTable with a single row to bind to DataGridView
-                var singleRowTable = dataRow.Table.Clone(); // Create an empty DataTable with the same schema
-                singleRowTable.ImportRow(dataRow); // Import the DataRow
-                dgvItems.DataSource = singleRowTable;
-            }
-            else
-            {
-                // Handle case when no item is found
-                MessageBox.Show("No item found with the specified criteria.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                // Try to parse the quantity value
+                if (int.TryParse(row.Cells["quantity"].Value.ToString(), out quantity))
+                {
+                    // Calculate the total price and update the total price cell
+                    row.Cells["netAmount"].Value = itemPrice * quantity;
+                }
+                else
+                {
+                    // If parsing fails, reset the quantity to 1 and update the total price
+                    row.Cells["quantity"].Value = 1;
+                    row.Cells["netAmount"].Value = itemPrice;
+                }
             }
         }
 
-        private void SearchUnkownValue()
+        private void LoadCategories()
         {
-            string itemName = txtByName.Text;
-            string itemBarcode = txtBarcode.Text;
-
-            var parameters = new
+            try
             {
-                itemName = string.IsNullOrEmpty(itemName) ? (string?)null : itemName,
-                itemBarcode = string.IsNullOrEmpty(itemBarcode) ? (string?)null : itemBarcode
-            };
+                var dataTable = _dapperService.QueryDataTable(Queries.GetCategories);
 
-            var dataTable = _dapperService.QueryDataTable(Queries.GetItemByNameOrBarcodeUnKown, parameters);
-
-            if (dataTable.Rows.Count > 0)
-            {
-                dgvItems.DataSource = dataTable;
+                if (dataTable.Rows.Count > 0)
+                {
+                    cboCategories.DisplayMember = "catName";
+                    cboCategories.ValueMember = "catCode";
+                    cboCategories.DataSource = dataTable;
+                }
+                else
+                {
+                    MessageBox.Show("No categories found.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
             }
-            else
+            catch (Exception ex)
             {
-                MessageBox.Show("No item found with the specified criteria.", "Information", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                MessageBox.Show($"An error occurred while loading categories: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
             }
-        }
-
-        private void btnSearchRange_Click(object sender, EventArgs e)
-        {
-            SearchUnkownValue();
         }
     }
 }
